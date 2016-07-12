9ce6a29ba50da475ab810642ff80fc6461da2566
vesper <bartek.bierla@gmail.com>
2016-07-12 16:59:28 +0200

    Add H2

---
+++ src/main/java/bb/controllers/MyController.java
@@ -0,0 +1,13 @@
+package bb.controllers;
+
+import bb.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by bartosz on 07.07.16.
+ */
+@Controller
+public class MyController {
+
+}
